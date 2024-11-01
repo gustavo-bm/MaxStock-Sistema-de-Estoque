8395abd67a15a77081ea3a97e4f817f4e42bac06
gustavo-bm <g.bianchinimoraes@gmail.com>
2024-11-01 20:09:11 -0300

    fix: migrations refeitas pois a dos usuários estava incompleta

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -16,7 +16,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: [User, Product],
     migrations: [CreateUsersTable1727265827543, CreateProductsTable1730304442738],
--- backend/src/database/migrations/1727265827543-CreateUsersTable.ts
+++ backend/src/database/migrations/1727265827543-CreateUsersTable.ts
@@ -1,11 +1,55 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUsersTable1727265827543 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        length: '100',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        length: '100',
+                        isNullable: false,
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        length: '100',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
     }
 
 }
